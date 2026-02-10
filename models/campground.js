042fdb25e1b873cdcefbff6192bf30ef561f5763
Tunde Lawal <babatundesodiqlawal@gmail.com>
2026-02-10 17:06:04 +0100

    Creating a logic middleware that listen for a delete event on campground &&  delete all it  reviews from the  database

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const mongoose = require("mongoose");
+const Review = require("./review");
 
 const Schema = mongoose.Schema;
 
@@ -9,6 +10,18 @@ const CampGroundSchema = new Schema({
   description: String,
   stateDetails: String,
   imageUrl: String,
+  reviews: [{ type: Schema.Types.ObjectId, ref: "Review" }],
+});
+
+// This logic middlewares triggers after a immediately a campground is deleted,
+// it will also delete all the reviews associated with that campground
+// (findbyIdAndDelete triggers findOneAndDelete middleware)
+CampGroundSchema.post("findOneAndDelete", async (doc) => {
+  if (doc) {
+    await Review.deleteMany({
+      _id: { $in: doc.reviews },
+    });
+  }
 });
 
 module.exports = mongoose.model("Campground", CampGroundSchema);
